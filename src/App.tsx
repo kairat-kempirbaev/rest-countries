2e8c20b12a44f7e32a03eaf48396504a3074ddb3
kairat-kempirbaev <7647704+kairat-kempirbaev@users.noreply.github.com>
2022-10-16 01:47:15 -0700

    Search filter by *keyword* RegEx

--- src/App.tsx
+++ src/App.tsx
@@ -12,14 +12,14 @@ function App(): JSX.Element {
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
   const [error, setError] = useState<{ message: string, [key: string]: any } | null>(null);
 
-  const [filter, setFilter] = useState<number>(-1);
+  const [search, setSearch] = useState<string|null>(null);
+  const [region, setRegion] = useState<string|null>(null);
   useEffect(() => {
     if (localStorage.getItem("items") === null) {
       fetch(URL)
         .then(response => response.json())
         .then(
           (result) => {
-            console.log(result)
             setIsLoaded(true);
             setItems(result);
             localStorage.setItem('items', JSON.stringify(result));
@@ -38,6 +38,16 @@ function App(): JSX.Element {
     }
   }, [])
 
+  function displayRecords() {
+    let flags:Flags[] = items;
+    if (search?.length) {
+      flags = items.filter(item=>(item.name.common.toLowerCase().includes(search.toLowerCase())))
+    }
+    return flags.map((item, index) =>
+        <CountryComponent  name={item.name} flags={item.flags} capital={item.capital} region={item.region} population={item.population}></CountryComponent>
+      )
+  }
+
   if (error) {
     return <div>Error: {error.message}</div>;
   } else if (!isLoaded) {
@@ -48,16 +58,14 @@ function App(): JSX.Element {
       <div className='w-[100vw]'>
         <header className='flex p-5 px-10 justify-between mb-10'>
           <h1 className='font-bold text-lg bg-white'>Where in the world?</h1>
-          <div  className='mr-12'>
-            <input type="checkbox" name="dark-toggle" id="dark"   className='sr-only'/>
-            <label htmlFor="dark"><img src="/moon.svg" alt="" width='20px' height='20px' className='inline'/> Dark Mode</label>
+          <div className='mr-12'>
+            <input type="checkbox" name="dark-toggle" id="dark" className='sr-only' />
+            <label htmlFor="dark"><img src="/moon.svg" alt="" width='20px' height='20px' className='inline' /> Dark Mode</label>
           </div>
         </header>
-        <InputComponent></InputComponent>
+        <InputComponent setRegion={setRegion} search={search} setSearch={setSearch}></InputComponent>
         <main className='flex flex-wrap gap-3 bg-vlg'>
-          {items.map((item, index) =>
-            <CountryComponent name={item.name} flags={item.flags} capital={item.capital} region={item.region} population={item.population}></CountryComponent>
-          )}
+          {displayRecords()}
         </main>
       </div>
     )
--- src/InputComponent.tsx
+++ src/InputComponent.tsx
@@ -2,23 +2,21 @@
 import React, { useState } from 'react'
 import "./InputComponent.css"
 
-export default function InputComponent(): JSX.Element {
-    const [textInput, setTextInput] = useState("");
+export default function InputComponent(props:any): JSX.Element {
     const [display, setDisplay] = useState(1);
-    const [filter, setFilter] = useState("");
     const options = ["absolute", "hidden"]
     function incDisplay(){
         setDisplay((display + 1) % 2)
     }
     function clearFilter(e: any) {
         incDisplay()
-        console.log("event Clicked",display)
-        setFilter(e.target.getAttribute("data-name"));
+        props.setRegion(e.target.getAttribute("data-name"));
+        e.stopPropagation();
     }
 
     return (
         <div className='flex justify-around mb-4'>
-            <input type="text" value={textInput} onChange={(e) => setTextInput(e.target.value)} placeholder='Search for a country...' className='border-[1px] p-3 pl-12 w-[400px] bg-wht   rounded-lg' />
+            <input type="text" value={props.search} onChange={(e) => props.setSearch(e.target.value)} placeholder='Search for a country...' className='border-[1px] p-3 pl-12 w-[400px] bg-wht   rounded-lg' />
             <div className='flex flex-col w-[180px]'>
                 <button className='border-[1px] p-4 text-left bg-white rounded-xl cursor-pointer' onClick={incDisplay}> Filter by Region </button>
                 <div className={'border-[1px] p-4 mt-3 bg-white rounded-xl absolute w-[180px] top-[160px] ' + options[display]} onClick={event => clearFilter(event)}>
